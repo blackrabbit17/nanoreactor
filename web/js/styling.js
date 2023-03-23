8b590129221e6b1b2c705984165e1b3f1a3b1792
Alain Richardt <blackrabbit17@protonmail.com>
2023-03-23 21:10:37 +1300

    Working layout

---
+++ web/js/styling.js
@@ -0,0 +1,29 @@
+LAYOUT_ENGINES = 
+{
+    breadthfirst : {
+        name: 'breadthfirst',
+        directed: true,
+        padding: 10,
+        fit: true
+    },
+    grid : {
+        name: 'grid',
+    },
+    circle : {
+        name: 'circle',
+    },
+    cola: {
+        name: 'cola',
+        maxSimulationTime: 7000,
+        fit: true,
+    },
+    fcose: {
+        name: 'fcose',
+        quality: 'proof',
+        animate: true,
+        animationDuration: 3000,
+        fit: true,
+        nodeSeparation: 175,
+
+    }
+}
