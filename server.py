187c0057beaaf2fb48b23157e8235808e8cae5d8
Alain Richardt <blackrabbit17@protonmail.com>
2023-06-16 17:19:33 +1200

    Rename to docs to CI/CD works and adjust server

--- server.py
+++ server.py
@@ -3,28 +3,26 @@
 
 import os
 
+import pandas as pd
 from fastapi import FastAPI, Response
 from fastapi.staticfiles import StaticFiles
-import pandas as pd
-
 
 app = FastAPI()
 
-app.mount("/css", StaticFiles(directory="web/css"), name="css")
-app.mount("/img", StaticFiles(directory="web/img"), name="css")
-app.mount("/js", StaticFiles(directory="web/js"), name="css")
+app.mount("/css", StaticFiles(directory="docs/css"), name="css")
+app.mount("/img", StaticFiles(directory="docs/img"), name="css")
+app.mount("/js", StaticFiles(directory="docs/js"), name="css")
 
 
 @app.get("/")
 def root():
-    with open(os.path.join('web', 'index.html')) as fh:
+    with open(os.path.join("docs", "index.html")) as fh:
         data = fh.read()
     return Response(content=data, media_type="text/html")
 
 
 @app.get("/reactions/")
 def reactions():
-
-    df = pd.read_csv('data/all_tools_graph_final.csv')
+    df = pd.read_csv("data/all_tools_graph_final.csv")
 
     return Response(content=df.to_json(), media_type="application/json")
